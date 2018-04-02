613de9945e41f96a20e6aa2250e24cec0ed2cda6
前田欣耶 <yoppie@maedakinnoMacBook-puro.local>
2018-04-03 00:36:46 +0900

    create button component sample

---
+++ src/TestButton.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class TestButton extends Component{
+    render(){
+        return(
+            <button>ぼたん</button>
+        );
+    }
+}
+
+export default TestButton;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import TestButton from './TestButton'
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<TestButton />, document.getElementById('button'));
 registerServiceWorker();
